77cde608edc79f11f0a85aa64e3be1e0adeabd7e
Mufti Rahmat P <85667375+muftirp@users.noreply.github.com>
2022-10-03 19:57:23 +0700

    Add files via upload

---
+++ database/migrations/2022_10_02_111034_create_tb_berita_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_berita', function (Blueprint $table) {
+            $table->id('berita_id');
+            $table->string('judul');
+            $table->string('gambar')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_berita');
+    }
+};
